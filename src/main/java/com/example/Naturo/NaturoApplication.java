ec2e8e3b1d797ec9b463e625d5271ef230ac6628
Pisc09 <pisco8118@gmail.com>
2025-12-27 16:25:06 +0100

    Initial commit

---
+++ src/main/java/com/example/Naturo/NaturoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Naturo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Naturo/NaturoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Naturo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
